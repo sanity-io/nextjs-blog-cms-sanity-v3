ad4274a8a1d19fb3d2edb789e350ab13691ebe16
Rita <rita@sanity.io>
2022-10-14 14:27:26 +0200

    refactor: make background of preview always white and move method out of postPreview component

--- components/Preview/PostsPreview.tsx
+++ components/Preview/PostsPreview.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
+import { Card } from '@sanity/ui'
 
 export function PostsPreview(props: any) {
-  function productionUrl({ document }) {
-    const url = new URL('/api/preview', location.origin)
-    const secret = process.env.NEXT_PUBLIC_PREVIEW_SECRET
-    if (secret) {
-      url.searchParams.set('secret', secret)
-    }
+  return (
+    <Card scheme="light" style={{ width: '100%', height: '100%' }}>
+      <iframe style={{ width: '100%', height: '100%' }} src={getUrl(props)} />
+    </Card>
+  )
+}
 
-    url.searchParams.set('slug', document.displayed.slug?.current!)
-
-    return url.toString()
+function getUrl({ document }) {
+  const url = new URL('/api/preview', location.origin)
+  const secret = process.env.NEXT_PUBLIC_PREVIEW_SECRET
+  if (secret) {
+    url.searchParams.set('secret', secret)
   }
 
-  return (
-    <iframe
-      style={{ width: '100%', height: '100%' }}
-      src={productionUrl(props)}
-    />
-  )
+  url.searchParams.set('slug', document.displayed.slug?.current!)
+
+  return url.toString()
 }
