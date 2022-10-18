b89de639c2c8b23b04e699e535abf020442c314d
Rita <rita@sanity.io>
2022-10-18 12:58:46 +0200

    refactor: add default no slug message

--- components/Posts/PostsPreview.tsx
+++ components/Posts/PostsPreview.tsx
@@ -1,7 +1,18 @@
-import { Card } from '@sanity/ui'
+import { Card, Text } from '@sanity/ui'
 import React from 'react'
 
 export function PostsPreview(props: any) {
+  // if the document has no slug for the preview iframe
+  if (!props.document.displayed.slug) {
+    return (
+      <Card tone="primary" margin={5} padding={6}>
+        <Text align="center">
+          Please add a slug to the post to see the preview!
+        </Text>
+      </Card>
+    )
+  }
+
   return (
     <Card scheme="light" style={{ width: '100%', height: '100%' }}>
       <iframe style={{ width: '100%', height: '100%' }} src={getUrl(props)} />
