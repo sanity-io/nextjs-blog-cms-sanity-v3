7ee8eee13a3deb166680a151fd1eed65f82bb3d2
Rita <rita@sanity.io>
2022-10-20 13:57:12 +0200

    fix: re-add title to index

--- components/blog-header.tsx
+++ components/blog-header.tsx
@@ -1,6 +1,6 @@
 import { CMS_NAME, CMS_URL } from '../lib/constants'
 
-export default function Intro({ title }) {
+export default function BlogHeader({ title }) {
   return (
     <section className="mt-16 mb-16 flex flex-col items-center md:mb-12 md:flex-row md:justify-between">
       <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
