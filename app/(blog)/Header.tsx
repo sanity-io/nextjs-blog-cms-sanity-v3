36a7bfc52dd0b7be23726a65a6831b12ff4d5d26
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-09 21:47:07 +0100

    chore: use manual suspense boundary

--- app/(blog)/Header.tsx
+++ app/(blog)/Header.tsx
@@ -14,11 +14,11 @@ export default async function Header({ level }: { level: 1 | 2 }) {
   switch (level) {
     case 1:
       return (
-        <header className="flex flex-col items-center mt-16 mb-10 md:mb-16 md:mb-12 md:flex-row md:justify-between">
+        <header className="mt-16 mb-10 flex flex-col items-center md:mb-16 md:mb-12 md:flex-row md:justify-between">
           <h1 className="text-6xl font-bold leading-tight tracking-tighter md:pr-8 md:text-8xl">
             {title}
           </h1>
-          <h4 className="mt-5 text-lg text-center md:pl-8 md:text-left">
+          <h4 className="mt-5 text-center text-lg md:pl-8 md:text-left">
             A statically generated blog example using{' '}
             <a
               href="https://nextjs.org/"
@@ -50,6 +50,10 @@ export default async function Header({ level }: { level: 1 | 2 }) {
       )
 
     default:
-      throw new Error(`Invalid level: ${JSON.stringify(level) || typeof level}, only 1 or 2 are allowed`)
+      throw new Error(
+        `Invalid level: ${
+          JSON.stringify(level) || typeof level
+        }, only 1 or 2 are allowed`
+      )
   }
 }
