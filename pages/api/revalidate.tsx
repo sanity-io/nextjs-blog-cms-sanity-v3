482d825b7099de653b062fb27d0b5ebee5f839db
Cody Olsen <stipsan@gmail.com>
2022-11-21 02:21:50 +0100

    feat: revalidate "More stories" in webhook (#89)
    Co-authored-by: Cody Olsen <81981+stipsan@users.noreply.github.com>

--- pages/api/revalidate.tsx
+++ pages/api/revalidate.tsx
@@ -20,92 +20,120 @@
  * 14. Redeploy with `npx vercel --prod` to apply the new environment variable
  */
 
-// @TODO move the heavy duty body parsing logic into `next-sanity/webhook`
-
-import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook'
 import { apiVersion, dataset, projectId } from 'lib/sanity.api'
-import { createClient } from 'next-sanity'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { type SanityClient, createClient, groq } from 'next-sanity'
+import { type ParseBody, parseBody } from 'next-sanity/webhook'
 
-// Next.js will by default parse the body, which can lead to invalid signatures
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
+export { config } from 'next-sanity/webhook'
 
-const AUTHOR_UPDATED_QUERY = /* groq */ `
-  *[_type == "author" && _id == $id] {
-    "slug": *[_type == "post" && references(^._id)].slug.current
-  }["slug"][]`
-const POST_UPDATED_QUERY = /* groq */ `*[_type == "post" && _id == $id].slug.current`
-const SETTINGS_UPDATED_QUERY = /* groq */ `*[_type == "post"].slug.current`
-
-const getQueryForType = (type) => {
-  switch (type) {
-    case 'author':
-      return AUTHOR_UPDATED_QUERY
-    case 'post':
-      return POST_UPDATED_QUERY
-    case 'settings':
-      return SETTINGS_UPDATED_QUERY
-    default:
-      throw new TypeError(`Unknown type: ${type}`)
-  }
-}
-
-const log = (msg, error?) =>
-  console[error ? 'error' : 'log'](`[revalidate] ${msg}`)
-
-async function readBody(readable) {
-  const chunks = []
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
-  }
-  return Buffer.concat(chunks).toString('utf8')
-}
-
-export default async function revalidate(req, res) {
-  const signature = req.headers[SIGNATURE_HEADER_NAME]
-  const body = await readBody(req) // Read the body into a string
-  if (
-    !isValidSignature(
-      body,
-      signature,
-      process.env.SANITY_REVALIDATE_SECRET?.trim()
-    )
-  ) {
-    const invalidSignature = 'Invalid signature'
-    log(invalidSignature, true)
-    res.status(401).json({ success: false, message: invalidSignature })
-    return
-  }
-
-  const jsonBody = JSON.parse(body)
-  const { _id: id, _type } = jsonBody
-  if (typeof id !== 'string' || !id) {
-    const invalidId = 'Invalid _id'
-    log(invalidId, true)
-    return res.status(400).json({ message: invalidId })
-  }
-
-  log('Wait a second to give Elastic Search time to reach eventual consistency')
-  await new Promise((resolve) => setTimeout(resolve, 1000))
-
-  log(`Querying post slug for _id '${id}', type '${_type}' ..`)
-  const client = createClient({ projectId, dataset, apiVersion, useCdn: false })
-  const slug = await client.fetch(getQueryForType(_type), { id })
-  const slugs = (Array.isArray(slug) ? slug : [slug]).map(
-    (_slug) => `/posts/${_slug}`
-  )
-  const staleRoutes = ['/', ...slugs]
-
+export default async function revalidate(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
+    const { body, isValidSignature } = await parseBody(
+      req,
+      process.env.SANITY_REVALIDATE_SECRET
+    )
+    if (isValidSignature === false) {
+      const message = 'Invalid signature'
+      console.log(message)
+      return res.status(401).send(message)
+    }
+
+    const { _id, _type } = body
+    if (typeof _id !== 'string' || !_id) {
+      const invalidId = 'Invalid _id'
+      console.error(invalidId, { body })
+      return res.status(400).send(invalidId)
+    }
+
+    const staleRoutes = await queryStaleRoutes({ _id, _type })
     await Promise.all(staleRoutes.map((route) => res.revalidate(route)))
+
     const updatedRoutes = `Updated routes: ${staleRoutes.join(', ')}`
-    log(updatedRoutes)
-    return res.status(200).json({ message: updatedRoutes })
+    console.log(updatedRoutes)
+    return res.status(200).send(updatedRoutes)
   } catch (err) {
-    log(err.message, true)
-    return res.status(500).json({ message: err.message })
+    console.error(err)
+    return res.status(500).send(err.message)
   }
+}
+
+type StaleRoute = '/' | `/posts/${string}`
+
+async function queryStaleRoutes(
+  body: Pick<ParseBody['body'], '_type' | '_id'>
+): Promise<StaleRoute[]> {
+  const client = createClient({ projectId, dataset, apiVersion, useCdn: false })
+
+  switch (body._type) {
+    case 'author':
+      return await queryStaleAuthorRoutes(client, body._id)
+    case 'post':
+      return await queryStalePostRoutes(client, body._id)
+    case 'settings':
+      return await queryAllRoutes(client)
+    default:
+      throw new TypeError(`Unknown type: ${body._type}`)
+  }
+}
+
+async function _queryAllRoutes(client: SanityClient): Promise<string[]> {
+  return await client.fetch(groq`*[_type == "post"].slug.current`)
+}
+
+async function queryAllRoutes(client: SanityClient): Promise<StaleRoute[]> {
+  const slugs = await _queryAllRoutes(client)
+
+  return ['/', ...slugs.map((slug) => `/posts/${slug}` as StaleRoute)]
+}
+
+async function mergeWithMoreStories(
+  client,
+  slugs: string[]
+): Promise<string[]> {
+  const moreStories = await client.fetch(
+    groq`*[_type == "post"] | order(date desc, _updatedAt desc) [0...3].slug.current`
+  )
+  if (slugs.some((slug) => moreStories.includes(slug))) {
+    const allSlugs = await _queryAllRoutes(client)
+    return [...new Set([...slugs, ...allSlugs])]
+  }
+
+  return slugs
+}
+
+async function queryStaleAuthorRoutes(
+  client: SanityClient,
+  id: string
+): Promise<StaleRoute[]> {
+  let slugs = await client.fetch(
+    groq`*[_type == "author" && _id == $id] {
+    "slug": *[_type == "post" && references(^._id)].slug.current
+  }["slug"][]`,
+    { id }
+  )
+
+  if (slugs.length > 0) {
+    slugs = await mergeWithMoreStories(client, slugs)
+    return ['/', ...slugs.map((slug) => `/posts/${slug}`)]
+  }
+
+  return []
+}
+
+async function queryStalePostRoutes(
+  client: SanityClient,
+  id: string
+): Promise<StaleRoute[]> {
+  let slugs = await client.fetch(
+    groq`*[_type == "post" && _id == $id].slug.current`,
+    { id }
+  )
+
+  slugs = await mergeWithMoreStories(client, slugs)
+
+  return ['/', ...slugs.map((slug) => `/posts/${slug}`)]
 }
