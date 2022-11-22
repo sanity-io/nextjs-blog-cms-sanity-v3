40598632e7696bc982831e7deb0256618746e15d
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-22 21:53:16 +0100

    fix: add post date to posts list view

--- schemas/post.ts
+++ schemas/post.ts
@@ -74,11 +74,16 @@ export default defineType({
     select: {
       title: 'title',
       author: 'author.name',
+      date: 'date',
       media: 'coverImage',
     },
-    prepare(selection) {
-      const { author } = selection
-      return { ...selection, subtitle: author && `by ${author}` }
+    prepare({ title, media, author, date }) {
+      const subtitles = [
+        author && `by ${author}`,
+        date && `on ${format(parseISO(date), 'LLL d, yyyy')}`,
+      ].filter(Boolean)
+
+      return { title, media, subtitle: subtitles.join(' ') }
     },
   },
 })
