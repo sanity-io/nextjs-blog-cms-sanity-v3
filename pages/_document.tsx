3aa37196183a2bb58abee5de1182b2421ab1c32c
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-24 05:26:28 +0100

    chore: ready

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="text-black bg-white">
+      <body className="bg-white text-black">
         <Main />
         <NextScript />
       </body>
