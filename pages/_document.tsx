ffa5dd8384b6774cbe67452c76579b9ec0979b50
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-24 05:19:13 +0100

    chore: refactor _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,13 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-import { ServerStyleSheetDocument } from 'next-sanity/studio'
 
-export default class Document extends ServerStyleSheetDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body className="bg-white text-black">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="text-black bg-white">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
