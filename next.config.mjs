3b4a99039c613bab49a6757c5a87131f1d8117a3
Cody Olsen <stipsan@gmail.com>
2022-11-28 10:52:29 +0100

    Revert "Revert "fix: `swcMinify` no longer breaks `FieldPresenceWithOverlay`" (#117)"
    This reverts commit 3b67bd08da37c682cb3baa51da78d11040189060.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const config = {
-  // @TODO turn swcMinify back on once the agressive dead code elimination bug that casues
-  // `ReferenceError: FieldPresenceWithOverlay is not defined` is fixed
-  swcMinify: false,
   experimental: {
     appDir: true,
   },
