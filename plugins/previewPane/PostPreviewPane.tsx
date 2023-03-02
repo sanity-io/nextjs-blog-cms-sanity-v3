9a62ea9c7908f71d59da86ba742f67b5caa81da4
Christopher Pecoraro <chrispecoraro@gmail.com>
2023-03-02 13:50:48 -0500

    Update PostPreviewPane.tsx

--- plugins/previewPane/PostPreviewPane.tsx
+++ plugins/previewPane/PostPreviewPane.tsx
@@ -21,7 +21,7 @@ type Props = {
 
 export default function PostPreviewPane(props: Props) {
   const { previewSecretId, apiVersion } = props
-  // Whenever the slug changes, wait a 3 seconds for elastic search to reach eventual consistency.
+  // Whenever the slug changes, wait 3 seconds for elastic search to reach eventual consistency.
   // This helps to prevent displaying "Invalid slug" or returning 404 errors while editing the slug manually.
   const [slug, setSlug] = useState(props.slug)
   useEffect(() => {
