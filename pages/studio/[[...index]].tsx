80eba3c4b9d706867b3314d8929286b2281d0c42
Cody Olsen <stipsan@gmail.com>
2023-04-28 15:23:00 +0200

    fix: use the `metadata` API

--- pages/studio/[[...index]].tsx
+++ pages/studio/[[...index]].tsx
@@ -1,27 +1,17 @@
 import Head from 'next/head'
 import { NextStudio } from 'next-sanity/studio'
-import { NextStudioHead } from 'next-sanity/studio/head'
-import { StudioLayout, StudioProvider } from 'sanity'
+import { metadata } from 'next-sanity/studio/metadata'
 import config from 'sanity.config'
-import { createGlobalStyle } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle(({ theme }) => ({
-  html: { backgroundColor: theme.sanity.color.base.bg },
-}))
 
 export default function StudioPage() {
   return (
     <>
       <Head>
-        <NextStudioHead favicons={false} />
+        {Object.entries(metadata).map(([key, value]) => (
+          <meta key={key} name={key} content={value} />
+        ))}
       </Head>
-
-      <NextStudio config={config}>
-        <StudioProvider config={config}>
-          <GlobalStyle />
-          <StudioLayout />
-        </StudioProvider>
-      </NextStudio>
+      <NextStudio config={config} />
     </>
   )
 }
