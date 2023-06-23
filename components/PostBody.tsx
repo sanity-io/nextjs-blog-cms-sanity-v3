4aeee7a6c1b20a8ce5c78eb19e2847b1812cfa7e
Thomas Gram <thomas@codeshark.net>
2023-06-23 11:35:33 +0200

    Extend the PostBody's PortableText to render images, using the SanityImage component.

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -7,14 +7,26 @@
  * https://portabletext.org/
  *
  */
-import { PortableText } from '@portabletext/react'
+import {
+  PortableText,
+  type PortableTextReactComponents,
+} from '@portabletext/react'
 
 import styles from './PostBody.module.css'
+import { SanityImage } from './SanityImage'
+
+const myPortableTextComponents: Partial<PortableTextReactComponents> = {
+  types: {
+    image: ({ value }) => {
+      return <SanityImage {...value} />
+    },
+  },
+}
 
 export default function PostBody({ content }) {
   return (
     <div className={`mx-auto max-w-2xl ${styles.portableText}`}>
-      <PortableText value={content} />
+      <PortableText value={content} components={myPortableTextComponents} />
     </div>
   )
 }
