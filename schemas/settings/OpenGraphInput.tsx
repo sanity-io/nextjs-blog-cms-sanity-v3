001f36cca745141b87668599b5c4fc54c105a544
Cody Olsen <stipsan@gmail.com>
2023-11-17 01:55:27 +0100

    chore: fix build error

--- schemas/settings/OpenGraphInput.tsx
+++ schemas/settings/OpenGraphInput.tsx
@@ -6,17 +6,13 @@ import styled from 'styled-components'
 
 const OpenGraphPreview = lazy(() => import('./OpenGraphPreview'))
 
-const RatioSkeleton = styled(Skeleton).attrs({
-  radius: 3,
-  shadow: 1,
-  overflow: 'hidden',
-})`
+const RatioSkeleton = styled(Skeleton)`
   aspect-ratio: ${width} / ${height};
   height: 100%;
   width: 100%;
 `
 
-const fallback = <RatioSkeleton animated />
+const fallback = <RatioSkeleton animated radius={3} overflow="hidden" />
 
 export default function OpenGraphInput(props: ObjectInputProps) {
   const value = useDeferredValue(props.value)
