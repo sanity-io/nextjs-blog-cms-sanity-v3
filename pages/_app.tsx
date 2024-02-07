7ef7c3b7d5d3f5732e4e21a2a225b5457ce5e9f1
Cody Olsen <stipsan@gmail.com>
2024-02-07 15:18:46 +0100

    fix: use `next/dynamic` instead of `lazy`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,14 @@ import 'tailwindcss/tailwind.css'
 
 import { VisualEditing } from '@sanity/visual-editing/next-pages-router'
 import { AppProps } from 'next/app'
-import { lazy, Suspense } from 'react'
+import dynamic from 'next/dynamic'
 
 export interface SharedPageProps {
   draftMode: boolean
   token: string
 }
 
-const PreviewProvider = lazy(() => import('components/PreviewProvider'))
+const PreviewProvider = dynamic(() => import('components/PreviewProvider'))
 
 export default function App({
   Component,
