44a31833f1f86da0db71552f6c102f54dc3c8188
Solovoro <info@solovoro.ca>
2025-08-24 15:23:38 -0700

    chore(seo): switch to dynamic sitemap route and centralize service/city lists

---
+++ lib/solovoro.ts
@@ -0,0 +1,13 @@
+export const services = [
+  { slug: 'moving', name: 'Moving' },
+  { slug: 'plumbing', name: 'Plumbing' },
+  { slug: 'cleaning', name: 'Cleaning' },
+  { slug: 'roofing', name: 'Roofing' },
+];
+
+export const cities = [
+  { slug: 'toronto', name: 'Toronto' },
+  { slug: 'montreal', name: 'Montreal' },
+  { slug: 'vancouver', name: 'Vancouver' },
+  { slug: 'calgary', name: 'Calgary' },
+];
